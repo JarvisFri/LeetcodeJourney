a0a89c90244bf717bcc2440d0e653d2054b81215
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-10 19:40:41 +0530

    Time: 141 ms (12.56%), Space: 144.9 MB (13.34%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,46 +1,44 @@
-class Solution { 
+class Solution {
 public:
-    // Check if the given string is a palindrome.
-    bool palindrome_check(const string &s){
-        int n = s.size();
-        for (int i = 0; i < n / 2; i++) {
-            if (s[i] != s[n - 1 - i]) 
-                return false;
+
+    //Decision: "a ab", "aa b"
+    //Subproblem: Suffix array: f(arr, i) 
+    //Recurrance: f(arr,i )= i U f(arr, i+1)+ i i+1 f(arr, i+2)...f(arr, n)
+    //                      Check if i, i i+1 .. are palandrome
+    //Base Case: i==n --> push curr to res
+
+    //aaaab: [a] aaab
+
+    bool isPalandrome(string s){
+        int n=s.size();
+
+        for(int i=0; i<(n/2); i++ ){
+            if(s[i]!=s[n-1-i]) return false;
         }
         return true;
     }
-    
-    // Recursive function with two pointers: start_ptr and end_ptr.
-    void f(const string &s, int start_ptr, int end_ptr, vector<string> & curr, vector<vector<string>> &res) {
-        int n = s.size();
-        
-        // Base Case: when start_ptr reaches the end, add the current partition.
-        if (start_ptr == n) {
+
+    void f(string s, int i, vector<string> curr, vector<vector<string>>& res){
+        //Base Case
+        int n=s.size();
+        if(i==n){
             res.push_back(curr);
             return;
         }
-        
-        // Out-of-bound check for end_ptr.
-        if (end_ptr > n) 
-            return;
-        
-        // Decision 1: Do not partition at the current end_ptr, i.e. extend the current substring.
-        f(s, start_ptr, end_ptr + 1, curr, res);
-        
-        // Decision 2: Partition by taking the substring from start_ptr to end_ptr - 1.
-        string temp = s.substr(start_ptr, end_ptr - start_ptr);
-        if (palindrome_check(temp)) {
-            curr.push_back(temp);
-            // After partitioning, start new substring from end_ptr.
-            f(s, end_ptr, end_ptr + 1, curr, res);
-            curr.pop_back();
+
+        //Recurrance
+        for(int j=i+1; j<=n; j++){
+            if(isPalandrome(s.substr(i,j-i))){
+                curr.push_back(s.substr(i,j-i));
+                f(s,j,curr,res);
+                curr.pop_back();
+            }
         }
     }
-    
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> curr;
-        f(s, 0, 1, curr, res);
+        f(s,0,curr,res);
         return res;
     }
 };
