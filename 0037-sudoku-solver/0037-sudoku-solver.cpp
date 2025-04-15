e7474779bd8ba54e5fa394fe265375cff01a3535
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-16 00:40:01 +0530

    Time: 582 ms (5.93%), Space: 8.9 MB (29.29%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,59 +1,60 @@
 class Solution {
 public:
-    // Helper: backtracking function that fills in empty cells.
-    bool backtrack(vector<vector<char>>& board, int pos,
-                   vector<pair<int, int>>& empties,
-                   bool rowUsed[9][10], bool colUsed[9][10], bool boxUsed[9][10]) {
-        // Base case: if we have placed numbers in all empty cells, we are done.
-        if (pos == empties.size())
-            return true;
+    bool sudokuChecker(vector<vector<char>>&board,int i, int j, int k){
+        //Check if k is valid
+        int row,col;
         
-        int i = empties[pos].first;
-        int j = empties[pos].second;
-        int boxIndex = (i / 3) * 3 + (j / 3);
-        
-        // Try placing each number 1-9.
-        for (int num = 1; num <= 9; num++) {
-            if (!rowUsed[i][num] && !colUsed[j][num] && !boxUsed[boxIndex][num]) {
-                // Place the number.
-                board[i][j] = num + '0';
-                rowUsed[i][num] = colUsed[j][num] = boxUsed[boxIndex][num] = true;
-                
-                // Recursively solve the rest.
-                if (backtrack(board, pos + 1, empties, rowUsed, colUsed, boxUsed))
-                    return true;  // Found a valid solution.
-                
-                // Undo the move.
-                board[i][j] = '.';
-                rowUsed[i][num] = colUsed[j][num] = boxUsed[boxIndex][num] = false;
+        for(int n=0; n<9; n++){
+            if(board[i][n]!='.'){
+                if(board[i][n]-'0'==k) return false;
             }
+            if(board[n][j]!='.'){
+                if(board[n][j]-'0'==k) return false;
+            }
+            row = (i / 3) * 3 + n / 3;
+            col = (j / 3) * 3 + n % 3;
+            if(board[row][col]!='.'){
+                if(board[row][col]-'0'==k) return false;
+            }
+
         }
-        return false;  // Trigger backtracking.
+        return true;
+
     }
-    
-    void solveSudoku(vector<vector<char>>& board) {
-        // Trackers for rows, columns, and 3×3 sub-boxes.
-        bool rowUsed[9][10] = { false };
-        bool colUsed[9][10] = { false };
-        bool boxUsed[9][10] = { false };
-        vector<pair<int, int>> empties;
-        
-        // Initialize trackers and record positions of empty cells.
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    empties.push_back({i, j});
-                } else {
-                    int num = board[i][j] - '0';
-                    rowUsed[i][num] = true;
-                    colUsed[j][num] = true;
-                    int boxIndex = (i / 3) * 3 + (j / 3);
-                    boxUsed[boxIndex][num] = true;
-                }
-            }
+    void f(vector<vector<char>>&board,int p,vector<pair<int,int>> &unsolvedIdx,vector<vector<char>>&ans){
+        //Base
+        int n=unsolvedIdx.size();
+        if (p==n){
+            ans=board;
+            return;
         }
         
-        // Start backtracking from the first empty cell.
-        backtrack(board, 0, empties, rowUsed, colUsed, boxUsed);
+        //REcurrance
+        int i=unsolvedIdx[p].first;
+        int j=unsolvedIdx[p].second;
+        for(int k=1; k<10; k++){
+            if(board[i][j]=='.' && sudokuChecker(board,i,j,k)){
+                board[i][j]='0'+k;
+                f(board,p+1,unsolvedIdx,ans);
+                board[i][j]='.';
+            }
+        }
+
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        vector<vector<char>> boardTemp=board;
+        vector<pair<int,int>> unsolvedIdx;
+        int unsolved_count=0;
+        for(int i=0; i<9;i++){
+            for(int j=0; j<9; j++){
+
+                if(board[i][j]=='.') unsolvedIdx.push_back(make_pair(i,j));
+                //
+            }
+        }
+
+        f(boardTemp,0,unsolvedIdx,board);
+        return;
+        
     }
 };
