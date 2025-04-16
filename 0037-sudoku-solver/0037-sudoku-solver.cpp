a1019378ab7bc54f036ab827744ce01de6ed0c67
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-16 07:43:35 +0530

    Time: 179 ms (90.62%), Space: 9 MB (10.67%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
-    bool sudokuChecker(vector<vector<char>>&board,int i, int j, int k){
-        //Check if k is valid
-        int row,col;
-        
-        for(int n=0; n<9; n++){
-            if(board[i][n]!='.'){
-                if(board[i][n]-'0'==k) return false;
-            }
-            if(board[n][j]!='.'){
-                if(board[n][j]-'0'==k) return false;
-            }
-            row = (i / 3) * 3 + n / 3;
-            col = (j / 3) * 3 + n % 3;
-            if(board[row][col]!='.'){
-                if(board[row][col]-'0'==k) return false;
-            }
 
-        }
-        return true;
-
-    }
-    void f(vector<vector<char>>&board,int p,vector<pair<int,int>> &unsolvedIdx,vector<vector<char>>&ans){
+void f(vector<vector<char>>&board,int p,vector<pair<int,int>> &unsolvedIdx,vector<vector<char>>&ans, vector<vector<int>>&rowFilled, vector<vector<int>>&colFilled, vector<vector<int>>&boxFilled){
         //Base
         int n=unsolvedIdx.size();
         if (p==n){
@@ -33,9 +13,13 @@ public:
         int i=unsolvedIdx[p].first;
         int j=unsolvedIdx[p].second;
         for(int k=1; k<10; k++){
-            if(board[i][j]=='.' && sudokuChecker(board,i,j,k)){
+            if(board[i][j]=='.' ){
+                int boxIdx=(i/3)*3+j/3;
+                if(rowFilled[i][k]||colFilled[j][k]||boxFilled[boxIdx][k]) continue;
                 board[i][j]='0'+k;
-                f(board,p+1,unsolvedIdx,ans);
+                rowFilled[i][k]=colFilled[j][k]=boxFilled[boxIdx][k]=1;
+                f(board,p+1,unsolvedIdx,ans,rowFilled,colFilled, boxFilled );
+                rowFilled[i][k]=colFilled[j][k]=boxFilled[boxIdx][k]=0;
                 board[i][j]='.';
             }
         }
@@ -44,16 +28,30 @@ public:
     void solveSudoku(vector<vector<char>>& board) {
         vector<vector<char>> boardTemp=board;
         vector<pair<int,int>> unsolvedIdx;
+
+        vector<vector<int>> rowFilled (9,vector<int>(10,0));
+        vector<vector<int>> colFilled (9,vector<int>(10,0));
+        vector<vector<int>> boxFilled (9,vector<int>(10,0));
+
+
+
         int unsolved_count=0;
         for(int i=0; i<9;i++){
             for(int j=0; j<9; j++){
-
+                int row=(i/3)*3+j/3;
+                // int col=(j%3)*3+j%3;
+                
                 if(board[i][j]=='.') unsolvedIdx.push_back(make_pair(i,j));
-                //
+                else{
+                    int nums=board[i][j]-'0';
+                    rowFilled[i][nums]=1;
+                    colFilled[j][nums]=1;
+                    boxFilled[row][nums]=1;
+                }
             }
         }
 
-        f(boardTemp,0,unsolvedIdx,board);
+        f(boardTemp,0,unsolvedIdx,board, rowFilled, colFilled, boxFilled);
         return;
         
     }
