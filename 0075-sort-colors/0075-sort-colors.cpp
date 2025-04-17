eedddce03daba60a40bbb961ae58bada7d5e5e6a
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-17 16:26:29 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.7%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int minIdx, temp;
+        //sol1
+        //int minIdx, temp;
         // for(int i=0; i<nums.size(); i++){
         //     minIdx=i;
         //     for(int j=i+1; j<nums.size(); j++){
@@ -13,38 +14,50 @@ public:
         //     nums[minIdx]=temp;
 
         // }
-        int countOnes=0 , countZeros=0, countTwos=0;
 
-        for(int i=0; i<nums.size(); i++){
-            switch(nums[i]){
-                case 1: 
-                    countOnes++;
-                    break;
-                case 2:
-                    countTwos++;
-                    break;
-                case 0:
-                    countZeros++;
+        //Sol2
+        // int countOnes=0 , countZeros=0, countTwos=0;
+
+        // for(int i=0; i<nums.size(); i++){
+        //     switch(nums[i]){
+        //         case 1: 
+        //             countOnes++;
+        //             break;
+        //         case 2:
+        //             countTwos++;
+        //             break;
+        //         case 0:
+        //             countZeros++;
                     
 
-            }
-        }
-        for(int i=0; i<nums.size(); i++){
+        //     }
+        // }
+        // for(int i=0; i<nums.size(); i++){
             
-            if(countZeros){
-                nums[i]=0;
-                countZeros--;
-            }
-            else if(countOnes) {
-                nums[i]=1;
-                countOnes--;
-                }
-            else if(countTwos){
-                nums[i]=2;
-                countTwos--;
-            }
+        //     if(countZeros){
+        //         nums[i]=0;
+        //         countZeros--;
+        //     }
+        //     else if(countOnes) {
+        //         nums[i]=1;
+        //         countOnes--;
+        //         }
+        //     else if(countTwos){
+        //         nums[i]=2;
+        //         countTwos--;
+        //     }
 
+        // }
+
+        int a=0,b=0,c=nums.size()-1;
+        while(b<=c){
+            if(nums[b]==0) swap(nums[b++],nums[a++]);
+            else if(nums[b]==1) b++;
+            else swap(nums[b],nums[c--]);
         }
+
+
+
         
     }
 };
