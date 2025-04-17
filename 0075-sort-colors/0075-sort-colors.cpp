2b046865cbecd30591fb5e1b1c54d4bde8aec7c0
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-17 15:54:46 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,15 +2,47 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int minIdx, temp;
+        // for(int i=0; i<nums.size(); i++){
+        //     minIdx=i;
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if(nums[minIdx]>nums[j]) minIdx=j;
+
+        //     }
+        //     temp=nums[i];
+        //     nums[i]=nums[minIdx];
+        //     nums[minIdx]=temp;
+
+        // }
+        int countOnes=0 , countZeros=0, countTwos=0;
+
         for(int i=0; i<nums.size(); i++){
-            minIdx=i;
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[minIdx]>nums[j]) minIdx=j;
+            switch(nums[i]){
+                case 1: 
+                    countOnes++;
+                    break;
+                case 2:
+                    countTwos++;
+                    break;
+                case 0:
+                    countZeros++;
+                    
 
             }
-            temp=nums[i];
-            nums[i]=nums[minIdx];
-            nums[minIdx]=temp;
+        }
+        for(int i=0; i<nums.size(); i++){
+            
+            if(countZeros){
+                nums[i]=0;
+                countZeros--;
+            }
+            else if(countOnes) {
+                nums[i]=1;
+                countOnes--;
+                }
+            else if(countTwos){
+                nums[i]=2;
+                countTwos--;
+            }
 
         }
         
