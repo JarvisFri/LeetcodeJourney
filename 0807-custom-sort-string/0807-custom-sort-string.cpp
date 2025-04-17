0f21f52690f1d9e2c4518ae758a696700ca3fb68
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-17 19:10:27 +0530

    Time: 1 ms (24.03%), Space: 8.6 MB (11.61%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,18 @@
+vector<int> orderIdx(26, INT_MAX);
+bool compare(char & a, char & b){
+return orderIdx[a-'a']<orderIdx[b-'a'];
+}
+
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        
+        
+        for(int i=0; i<order.size(); i++){
+            orderIdx[order[i]-'a']=i;
+        }
+        sort(s.begin(), s.end(), compare);
+        return s;
+    }
+};
