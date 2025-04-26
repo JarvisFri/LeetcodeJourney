bafdbd70c384bb8161daacbe82e1bbde29437e72
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-26 12:32:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (81.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -39,8 +39,37 @@ public:
     }
     int numDecodings(string s) {
         int n=s.size();
-        vector<pair<int,int>> dp(n,{-1,-1});
-        return countDecoding(s,0,0,dp)+countDecoding(s,0,1,dp);
+        // vector<pair<int,int>> dp(n,{-1,-1});
+        
+        //Bottomup 
+        //DnC : f(s,i)=f(s,i-1)+f(s,i-2) //i-1: single digit, i-2: double digit
+        //Subproblem: prefix subarray
+        vector<int> dp(n,0);
+
+        dp[0]=s[0]=='0'?0:1;
+        if(n==1) return dp[0];
+        //Single:
+        if(s[1]!='0') dp[1]+=dp[0];
+
+        if(s[0]=='1' || (s[0]=='2' && s[1]<='6')) dp[1]+=1;
+
+
+        for(int i=2; i<n; i++){
+            //Single digit
+            if(s[i]!='0')
+                dp[i]+=dp[i-1];
+            //Double gigit
+            if(s[i-1]=='1')
+                dp[i]+=dp[i-2];
+            if(s[i-1]=='2' && s[i]<='6')
+                dp[i]+=dp[i-2];
+        }
+
+        return dp[n-1];
+
+
+
+        // return countDecoding(s,0,0,dp)+countDecoding(s,0,1,dp);
         
     }
 };
