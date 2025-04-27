629818a1a7ec8b90b46bb6ea0f9efed3a14a264c
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-27 09:26:45 +0530

    Time: 146 ms (5.18%), Space: 10.7 MB (19.52%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,34 +3,65 @@ public:
 
 
     int longestValidParentheses(string s) {
-        int n=s.size(), maxValid=0, count;
-        stack<char> temp;
-        for(int i=0; i<n; i++ ){
-            count=0;
-            
-            while (!temp.empty()) {
-                    temp.pop();
-                }
 
-            for(int j=i; j>=0; j--){
-                if(s[j]==')'){
-                    temp.push(')');
-                    count++;
-                    // cout<<count<<" )  \n";
-                }     
-                else{
-                    if(!temp.empty()){
-                        temp.pop();
-                        count++;
-                        // cout<<count<<" ( \n";
-                        if(temp.empty()) maxValid=max(count,maxValid);
-                    }else break; 
+        //s(i)= 0 if ')'
+        //else s(i)=s(i-1)+2+s(s(i-2)-2)
 
+        int n=s.size(), res=0;
+
+        vector<int> dp(n,0);
+
+        if(n==0) return 0;
+
+
+        dp[0]=0;
+        for(int i=1; i<n; i++){
+            if(s[i]=='(')
+                dp[i]=0;
+            else{
+                if((i-dp[i-1]-1)>=0 && s[i-dp[i-1]-1]=='('){
+                    dp[i]+=dp[i-1]+2;
+                if((i-dp[i-1]-2)>=0)
+                    dp[i]+=dp[i-dp[i-1]-2];
                 }
-                
             }
-        }
 
-        return maxValid;
+            res=max(res, dp[i]);
+            cout<<dp[i]<<" ";
+        }
+        
+        return res;
+        
+
+        //Method 1: o(n^2)
+        // int n=s.size(), maxValid=0, count;
+        // stack<char> temp;
+        // for(int i=0; i<n; i++ ){
+        //     count=0;
+            
+        //     while (!temp.empty()) {
+        //             temp.pop();
+        //         }
+
+        //     for(int j=i; j>=0; j--){
+        //         if(s[j]==')'){
+        //             temp.push(')');
+        //             count++;
+        //             // cout<<count<<" )  \n";
+        //         }     
+        //         else{
+        //             if(!temp.empty()){
+        //                 temp.pop();
+        //                 count++;
+        //                 // cout<<count<<" ( \n";
+        //                 if(temp.empty()) maxValid=max(count,maxValid);
+        //             }else break; 
+
+        //         }
+                
+        //     }
+        // }
+
+        // return maxValid;
     }
 };
