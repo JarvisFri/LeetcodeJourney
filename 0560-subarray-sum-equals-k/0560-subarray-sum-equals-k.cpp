a753e873881bc1ff1195b8e9082253515dc615bb
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-29 01:12:13 +0530

    Time: 1332 ms (23.39%), Space: 50.9 MB (23.8%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    //Idea: Let Sum from 0 to j be K'
+    //Lets try to calculate all such i for which array ends at j and sum is K
+    //This is same as calculating all prefix subarrays of i with sum K'-K
+    //Store in hashtable <sum, freq of sum>
+    // Do above step for each j
+    int subarraySum(vector<int>& nums, int k) {
+        int sumTilli=0, sumTillj=0, count=0, currSum;
+
+        unordered_map<int,int> sumFreq;
+        sumFreq[0]=1;
+
+        sumTilli=0;
+        for(int j=0; j<nums.size(); j++){
+            
+            sumTillj+=nums[j];
+            //Freq till one elt before j
+            
+            if(j==0  ) sumTillj==k?count++:0;
+            else{
+                sumTilli=sumTillj-k;
+                count+=sumFreq[sumTilli];
+            }
+            
+            
+            sumFreq[sumTillj]++;
+            cout<<sumTillj<<" "<<sumTilli<<" "<<count<<endl;
+   
+        } 
+
+        return count;
+        
+    }
+};
