877f5bc780395ea1adbab4f39582384effe80398
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-28 15:09:16 +0530

    Time: 4 ms (8.84%), Space: 13.3 MB (13.88%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+    //Rabbits of same group will observe the same num
     int numRabbits(vector<int>& answers) {
 
         //Count freq of ans
         //Lets store all ans in an map
-        unordered_map<int,int> ansFreq;
+        unordered_map<float,float> ansFreq;
         int minCount=0, multiplier;
 
         for(int i=0; i<answers.size(); i++) ansFreq[answers[i]]++;
 
         for(auto pair: ansFreq){
             multiplier=0;
-            if((pair.second)%(pair.first+1)) multiplier= 1+ (pair.second)/(pair.first+1);
-            else multiplier= (pair.second)/(pair.first+1);
+            multiplier= ceil((pair.second)/(pair.first+1));
 
             minCount+=multiplier*(pair.first+1);
         }
