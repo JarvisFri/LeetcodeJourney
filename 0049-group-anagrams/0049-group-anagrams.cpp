7406df7da95ca3c6054be9c06053f960d4352d45
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-28 17:27:52 +0530

    Time: 15 ms (72.35%), Space: 23.6 MB (95.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
 //Group identifier : way to identify string of same group
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    map< vector<int>, int> anagram_map;
+    unordered_map< string, int> anagram_map;
     vector<vector<string>> res;
-    vector<int> key(26,0);
+    // vector<int> key(26,0);
 
     for (string str : strs) {
-        // string sorted_str = str;
-        // sort(sorted_str.begin(), sorted_str.end());
-        for(char  c: str) key[c-'a']++;
+        string sorted_str = str;
+        sort(sorted_str.begin(), sorted_str.end());
+        // for(char  c: str) key[c-'a']++;
         
-        if(anagram_map.find(key)!=anagram_map.end())
-            res[anagram_map[key]].push_back(str);
+        if(anagram_map.find(sorted_str)!=anagram_map.end())
+            res[anagram_map[sorted_str]].push_back(str);
         else{
             res.push_back({str});
-            anagram_map[key]=res.size()-1;
+            anagram_map[sorted_str]=res.size()-1;
         }
-        fill(key.begin(), key.end(),0);
+        // fill(key.begin(), key.end(),0);
     }
 
     // for (auto& pair : anagram_map) {
