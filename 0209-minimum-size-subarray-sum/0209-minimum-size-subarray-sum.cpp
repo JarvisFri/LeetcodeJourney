9845aeb60f08b7cc02c10238c124f7761a07ebcc
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-29 12:02:28 +0530

    Time: 8 ms (5.28%), Space: 34.2 MB (5.05%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,46 +4,38 @@ public:
     //for each j find a number i in vector using bs such that num i > sumj-k
     //store minimum length if found
 
-    int bs(unordered_map<int,int> &sumIdx, int target, int lo, int hi){
+    int bs(vector<int> & arr, int target){
         //Bs to find greatest elt <=target: T,T,T',F,F
-        // int lo=0, hi=arr.size()-1;
-        int mid;
+        int lo=0, hi=arr.size()-1,mid;
 
         while(lo<hi){
             mid=lo+(hi-lo+1)/2;
-            if(sumIdx[mid]<=target)
+            if(arr[mid]<=target)
                 lo=mid;
             else
                 hi=mid-1;
         }
 
-        if (sumIdx[lo]<=target) return lo;
-        return -2;
+        if (arr[lo]<=target) return lo;
+        return -1;
         
     }
     int minSubArrayLen(int target, vector<int>& nums) {
         //
-        // vector<int> arr;//Give memory limit
-        unordered_map<int, int> sumIdx;//key is idx value is sum
-
+        vector<int> arr;
         int n=nums.size(), sumTillj=0, len=INT_MAX,targetPrime, i;
         //For i=0 ie subarray 0 to j, arr shift right by 1 
-        // arr.push_back(0);
-        //Store idx 0 for sum 0
-        sumIdx[-1]=0;
+        arr.push_back(0);
 
         for(int j=0; j<n; j++){
             sumTillj+=nums[j];
             targetPrime=sumTillj-target;
-            i=bs(sumIdx, targetPrime, -1, j-1);
-            if(i!=-2){
-                len=min(len,j-i);
-
-
+            i=bs(arr, targetPrime);
+            if(i!=-1){
+                len=min(len,j-i+1);
             } 
 
-            // arr.push_back(sumTillj);
-            sumIdx[j]=sumTillj;
+            arr.push_back(sumTillj);
 
         }
 
