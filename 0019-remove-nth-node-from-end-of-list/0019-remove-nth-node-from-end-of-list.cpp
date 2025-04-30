32acfb26bf69abc8038cd30bdfc0d808caec7ad4
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-04-30 13:46:49 +0530

    Time: 0 ms (100%), Space: 14.9 MB (59.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -13,30 +13,53 @@ public:
     //Traverse till end - get size
     // Nose to remove from start:temp= (size-n )->next
     //store (size-n )->next=(size-n)->next->next, free(temp)
+    //Method 2: Keep two pointer one n steps ahead of other, p2->next ==NULL, p1->next=p1->next->next
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int size=1;
-        ListNode* itr=head;
-        while(itr->next!=NULL){
-            itr=itr->next;
-            size++;
-        }
 
-        int idxToDel=size-n;//Next of this needs to be deleted
-        itr=head;
-        while(idxToDel>1){
-            itr=itr->next;
-            idxToDel--;
-        }
+        int gap=0;
 
-        if (idxToDel==0)
-            head=itr->next;
-        ListNode* temp=itr;
-        cout<<itr->val<<" ";
-        if(itr->next==NULL) return NULL;
-        itr->next=itr->next->next;
-        // free(temp);
+        ListNode *p1=head, *p2=head;
+
+        while(p2->next!=NULL){
+            if(gap<n){
+                p2=p2->next;
+                gap++;
+            }
+            else{
+                p1=p1->next;
+                p2=p2->next;
+            }
+        }
+        if(gap==n)
+        p1->next=p1->next->next;
+        else head=p1->next;
 
         return head;
+
+        // int size=1;
+        // ListNode* itr=head;
+        // while(itr->next!=NULL){
+        //     itr=itr->next;
+        //     size++;
+        // }
+
+        // int idxToDel=size-n;//Next of this needs to be deleted
+        // itr=head;
+        // while(idxToDel>1){
+        //     itr=itr->next;
+        //     idxToDel--;
+        // }
+
+        // if (idxToDel==0)
+        //     head=itr->next;
+        // ListNode* temp=itr;
+        // cout<<itr->val<<" ";
+        // if(itr->next==NULL) return NULL;
+        // itr->next=itr->next->next;
+        // // free(temp);
+
+        // return head;
 
 
 
