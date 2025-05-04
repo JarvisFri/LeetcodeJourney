d07330de99835813398c2e6fcaebf8c38ba71519
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-04 22:53:19 +0530

    Time: 52 ms (81%), Space: 21.3 MB (48.42%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -5,44 +5,38 @@ public:
         unordered_set<string> dictionary(wordList.begin(), wordList.end());
         
         unordered_set<string> visited;
-        int k=beginWord.size(), level=0;
+        int k=beginWord.size(), currlevel=0;
         string currStr,temp;
 
         //LevelOrderTravelsal:BFS
         //Nodes:GeneratenNodes:for each char traverse the string of size K
         //Edges:Lookup in Dict
 
-        queue<string> q;
+        queue<pair<string,int>> q;
         q.push({beginWord,0});
 
         while(!q.empty()){
             int sz=q.size();
+            
 
-            for(int i=0; i<sz; i++){
-
-            currStr=q.front();
-            // currlevel=q.front().second;
+            currStr=q.front().first;
+            currlevel=q.front().second;
             q.pop();
-            visited.insert(currStr);
+            // visited.insert(currStr);
             if(currStr==endWord)
-                return level+1;
+                return currlevel+1;
             //Generate all possible next nodes
             for(int i=0; i<k; i++){
                 temp=currStr;
                 for(int j=0; j<26; j++){
                     //Change the ith char of string
                     temp[i]='a'+j;
-                    if(visited.find(temp)==visited.end() && dictionary.find(temp)!=dictionary.end()){
-                        q.push(temp);
-                        //optimization: early exit
-                        if(temp==endWord)
-                        return level+2;
+                    if( dictionary.erase(temp)){
+                        q.push({temp,currlevel+1});
                     }
                 }
             }
 
-            }
-            level++;
 
 
         }
