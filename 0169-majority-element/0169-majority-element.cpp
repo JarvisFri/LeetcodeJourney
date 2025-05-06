9afa3c19338056faf0c5abd26e5b461f15386ff7
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-06 22:15:27 +0530

    Time: 4 ms (19.62%), Space: 28.2 MB (24.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> freqCount;
-        int maxElt=nums[0];
-
-        for(int i=0; i<nums.size(); i++){
-            freqCount[nums[i]]++;
-            if(freqCount[nums[i]]>freqCount[maxElt]) maxElt=nums[i];
-
+        //Method 2: Moores Algo
+        
+        int voteCount=0, candidate;
+        for(auto i : nums){
+            if(voteCount==0){
+                candidate=i;
+                voteCount++;
+            }else{
+                if(candidate==i) voteCount++;
+                else voteCount--;
+            }
         }
+        return candidate;
+        
+        //Method 1
+        // unordered_map<int,int> freqCount;
+        // int maxElt=nums[0];
 
-        return maxElt;
+        // for(int i=0; i<nums.size(); i++){
+        //     freqCount[nums[i]]++;
+        //     if(freqCount[nums[i]]>freqCount[maxElt]) maxElt=nums[i];
+
+        // }
+
+        // return maxElt;
+
+
     }
 };
