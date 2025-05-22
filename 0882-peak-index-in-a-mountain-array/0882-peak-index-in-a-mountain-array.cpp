4c94a3b165c0b14d59f96b0958866942aebe8f1d
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-22 12:04:28 +0530

    Time: 0 ms (100%), Space: 63.5 MB (47.92%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    int bs(vector<int>&arr){
-        int lo=0, hi=arr.size()-2;//arr.size()>2
-        int mid;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        //p(x): arr[x+1]>arr[x]
+        //TT*FFF*: First F
+
+        int lo=0, hi=arr.size()-1, mid;
 
         while(lo<hi){
             mid=lo+(hi-lo)/2;
-
-            if(arr[mid]>arr[mid+1]){
-                hi=mid;
-                
-
-            }else{
+            if(arr[mid+1]>arr[mid]){
                 lo=mid+1;
+            }else{
+                hi=mid;
             }
         }
-        int last_elt=arr[arr.size()-1];
-        if(arr[lo]<last_elt){
-            return arr.size()-1;
-        }
+
         return lo;
-
-    }
-    int peakIndexInMountainArray(vector<int>& arr) {
-
-        return bs(arr);
-        
     }
 };
