c2394bf56890297f79821f6d3cc2e4d135006a16
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-25 00:02:32 +0530

    Time: 1 ms (51.59%), Space: 10.3 MB (97.16%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,37 +3,46 @@ public:
     //Decision: Include or exclude
     //Subproblem: Suffix array
 
-    void f(vector<int>& nums, int i, vector<int>& currSet, set<vector<int>> &ans){
+    void f(vector<int>& nums, int i, vector<int>& currSet, vector<vector<int>> &ans){
         int n=nums.size();
         //Base case
-        if(i==n){
-            ans.insert(currSet);
-            return;
-        }
+        if(i>n) return;
+        // if(i==n){
+            ans.push_back(currSet);
+            // return;
+        // }
 
         //Recurrance
-        //Include
-        currSet.push_back(nums[i]);
-        f(nums, i+1, currSet, ans);
-        currSet.pop_back();
+        //All elt starting with i , i+1.. given all nums i uniquwe will avoid duplicates
+        for(int j=i; j<n; j++){
+            if(j!=i && nums[j]==nums[j-1]) continue;
+            currSet.push_back(nums[j]);
+            f(nums,j+1, currSet,ans);
+            currSet.pop_back();
+        }
+        // //Include
+        // currSet.push_back(nums[i]);
+        // f(nums, i+1, currSet, ans);
+        // currSet.pop_back();
 
-        //Exclude
-        f(nums, i+1, currSet,ans);
+        // //Exclude
+        // f(nums, i+1, currSet,ans);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> currSet;
         sort(nums.begin(), nums.end());
 
         f(nums,0, currSet, ans);
+        return ans;
 
-        vector<vector<int>> res;
-        for(auto subset: ans){
-            res.push_back(subset);
-        }
+        // vector<vector<int>> res;
+        // for(auto subset: ans){
+        //     res.push_back(subset);
+        // }
 
-        return res;
+        // return res;
 
     }
 };
