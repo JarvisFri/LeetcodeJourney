d54dd661f0b737cddcec919af326cd02ebd4c371
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-26 01:45:50 +0530

    Time: 32 ms (82.62%), Space: 53.4 MB (92.98%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
+    //Decision: Loop through the string and call recurrsive function for all valid substring to 
+    //partition from given partition and call recurrsive function
+    //Subproblem: Suffix array
+    //Prb and subprob: submission of partition
+    //Base Case: i=n;
 
-    //Decision: "a ab", "aa b"
-    //Subproblem: Suffix array: f(arr, i) 
-    //Recurrance: f(arr,i )= i U f(arr, i+1)+ i i+1 f(arr, i+2)...f(arr, n)
-    //                      Check if i, i i+1 .. are palandrome
-    //Base Case: i==n --> push curr to res
-
-    //aaaab: [a] aaab
-
-    bool isPalandrome(string s){
-        int n=s.size();
-
-        for(int i=0; i<(n/2); i++ ){
-            if(s[i]!=s[n-1-i]) return false;
+    bool isPalindrome(string &s, int start, int end ){
+        while(start<end){
+            if(s[start]!=s[end])
+                return false;
+            start++;
+            end--;
         }
         return true;
     }
 
-    void f(string s, int i, vector<string> &curr, vector<vector<string>>& res){
-        //Base Case
+
+    void f(string &s, int i, vector<string> &currStr, vector<vector<string>> &res){
         int n=s.size();
+        //Base
         if(i==n){
-            res.push_back(curr);
+            res.push_back(currStr);
             return;
         }
 
-        //Recurrance
+        //Recurrsive
         for(int j=i+1; j<=n; j++){
-            if(isPalandrome(s.substr(i,j-i))){
-                curr.push_back(s.substr(i,j-i));
-                f(s,j,curr,res);
-                curr.pop_back();
+            if(isPalindrome(s,i,j-1)){
+                currStr.push_back(s.substr(i,j-i));
+                f(s,j, currStr,res);
+                currStr.pop_back();
             }
         }
+
     }
+
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
-        vector<string> curr;
-        f(s,0,curr,res);
+        vector<string> currSet;
+
+        f(s,0,currSet,res);
+
         return res;
     }
 };
