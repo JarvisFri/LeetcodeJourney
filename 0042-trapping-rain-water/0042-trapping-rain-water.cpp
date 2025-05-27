db466ac82ebcf9f85c0477b38e50b008c9dda3b4
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-05-27 09:20:34 +0530

    Time: 0 ms (100%), Space: 25.8 MB (93.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    //Method2: 2Pointer: leftMax, rightMax
-    //Whichever is smaller increment that, left++, right--
-    
+    //Take two idx left and right and also left max and right max increment the one with lower max
     int trap(vector<int>& height) {
-        int leftMax=0,rightMax=0,left=0,right=height.size()-1,ans=0;
-        if(height.size()<=1)return 0;
-        leftMax=height[left];
-        rightMax=height[right];
+        int n=height.size();
+        int ans=0;
         
-        // left++;
-        // right--;
+        int left=0,right=n-1,leftMax=0, rightMax=0;
 
         while(left<=right){
             if(leftMax<rightMax){
-                if(height[left]>leftMax)
+                if(height[left]<leftMax)
+                    ans+=leftMax-height[left];
+                else
                     leftMax=height[left];
-                else ans+=leftMax-height[left];
                 left++;
             }else{
-                if(height[right]>rightMax)
+                if(height[right]<rightMax)
+                    ans+=rightMax-height[right];
+                else
                     rightMax=height[right];
-                else ans+=rightMax-height[right];
                 right--;
             }
         }
 
         return ans;
-
-       
-        
     }
 };
