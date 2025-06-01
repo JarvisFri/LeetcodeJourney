5eb63149c46a37342d6d0fbf233438c2775a3f95
JarvisFri <79247398+JarvisFri@users.noreply.github.com>
2025-06-01 19:57:03 +0530

    Time: 291 ms (60.1%), Space: 10.8 MB (60%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,59 +1,53 @@
-vector<vector<int>> dirn={ { -1,0 },{0,-1},{1,0},{0,1}};
-
 class Solution {
 public:
 
-    void f (vector<vector<char>> & board, vector<vector<int>> & visited, string word,int k, int i , int j, bool &res){
-        //Base case
-        int l=word.size(), m=board.size(), n=board[0].size();
+    vector<vector<int>> dirn={{-1,0},{+1,0},{0,-1},{0,+1}};
+
+    void f(vector<vector<char>>& board, vector<vector<int>>& visited, int i,int j, int k, string &word, bool & res){
+        int m=board.size();
+        int n=board[0].size();
+        int l=word.size();
+        //Base
+        //Already Found
+        if(res) return;
+        int i_next, j_next;
         //Positive
-        if(k==l){
+        if(k==l-1 && word[k]==board[i][j]){
             res=true;
             return;
         }
         //Negative
-        if(i<0||j<0||j==n||i==m) return;
-        
-        if(visited[i][j]) return;
-
         if(word[k]!=board[i][j]) return;
-
-     
-        //Call function further in all direction if dirn exist
-        //mark visited
-        visited[i][j]=1;
-        int new_i,new_j;
-        for(int p=0; p<dirn.size();p++){
-            new_i=i+dirn[p][0];
-            new_j=j+dirn[p][1];
-
-            
-                f(board,visited,word,k+1,new_i, new_j, res);
-                if(res)return;
-            
-
+        
+           
+        //Recurssive Step
+        for(int x=0; x<4; x++){
+            i_next=i+dirn[x][0];
+            j_next=j+dirn[x][1];
+            if(i_next>=0 && j_next>=0 && i_next<m && j_next<n && !visited[i_next][j_next] ){
+                //Mark Visited
+                visited[i_next][j_next]=1;
+                f(board,visited,i_next,j_next,k+1,word, res);
+                visited[i_next][j_next]=0;
+            }
         }
-        visited[i][j]=0;
-
+        
     }
+
     bool exist(vector<vector<char>>& board, string word) {
         bool res=false;
-        int l=word.size(), m=board.size(), n=board[0].size();
+        int m=board.size();
+        int n=board[0].size();
         vector<vector<int>> visited(m,vector<int>(n,0));
 
         for(int i=0; i<m; i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==word[0]){
-                    
-                    f(board,visited,word,0,i,j,res);
-                    
-                    if(res) return res;
-                }
+                visited[i][j]=1;
+                f(board,visited,i,j,0,word,res);
+                visited[i][j]=0;
             }
         }
 
         return res;
-
-        
     }
 };
